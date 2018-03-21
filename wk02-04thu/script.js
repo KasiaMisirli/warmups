f23039817f02850d9d961ea630c7d259b265ca8d
kasia-misirli <kasia.misirli@gmail.com>
2018-03-22 10:20:11 +1100

    adding hash wall

---
+++ wk02-04thu/script.js
@@ -0,0 +1,24 @@
+//must print out below effect:
+
+// Write a program that creates a string that represents 
+// an 8×8 grid, using new- line characters to separate lines. At
+//  each position of the grid there is either a space or a “#” character. 
+//  The characters should form a chess board. Passing this string to 
+//  console.log should show something like this:
+
+// # # # # # # # #
+//  # # # # # # # #
+// # # # # # # # #
+//  # # # # # # # #
+var space=" ";
+var hashLine= [];
+var symbol = " # ";
+ var hashTree = function(){
+    for(var i=0;i<8;i++){
+        hashLine.push(symbol);
+}
+    var str= hashLine.join("");
+    return str+"\n"+space+str+"\n"+str+"\n"+space+str+"\n"+str+"\n"+space+str+"\n"+str+"\n"+space+str;
+} 
+console.log(hashTree(hashLine));
+
